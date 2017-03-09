f2e0daa07408e60d794b03215c56bc5c19ec7f81
Sam Rausser <srausser@gmail.com>
2017-03-09 13:28:51 -0800

    Update configuration.py

--- lookerpy/configuration.py
+++ lookerpy/configuration.py
@@ -56,7 +56,7 @@ class Configuration(object):
         Constructor
         """
         # Default Base url
-        self.host = "https://looker.buffer.com:19999/api/3.0"
+        self.host = "https://hoteltonight.looker.com:19999/api/3.0"
         # Default api client
         self.api_client = None
         # Temp file folder for downloading files
